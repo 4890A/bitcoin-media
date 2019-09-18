c34bc88f216e6edeaddef4c6e1e1851a906b9910
d4890 <alexclass2015@gmail.com>
2019-09-18 14:11:24 -0400

    created outline for bubble chart

---
+++ reddit-participation/bubble_chart/js/bubble.js
@@ -0,0 +1,120 @@
+
+function createChart(){
+
+	var width = 1000;
+	var height = 1000;
+	
+	// the center coordinate which will be used as a 
+	// center attraction point for the bubbles
+	var center = {x : width / 2.,
+			y: height / 2. };
+
+	
+	// initialize svg area, bubbles and their nodes
+	var svg = null;
+	var bubbles = null;
+	var nodes = [];
+
+	// create repulsive force, as a function of node seperation
+	var forceStrength = 0.05;
+
+	function charge(distance) {
+		return - Math.pow(distance.radius, 2.0) * forceStrength;
+	}
+
+	var simulation = d3.forceSimulation()
+		// drag force that slows down bubbles
+		.velocityDecay(0.2)
+		// x and y forces that pull to the center
+		.force('x', d3.forceX().strength(forceStrength).x(center.x))
+		.force('y', d3.forceY().strength(forceStrength).y(center.y))
+		// repulsive force
+		.force('charge', d3.forceManyBody().strength(charge))
+		.on('tick', ticked);
+
+	// prevents simulation from running before nodes are created
+	simulation.stop();
+
+	function createNodes(data) {
+
+		// the frequency at which the most common word occurs
+		var maxAmount = d3.max(data, d => (+d.frequency));
+
+		// use a logarithmic scale for the bubble radius	
+		var radiusScale = d3.scalePow()
+			.exponent(0.5)
+			.range([2, 85])
+			.domain([0, maxAmount]);
+
+		var myNodes = data.map(d => (
+			{
+				name: d.word,
+				radius: radiusScale(+d.frequency),
+				value: +d.frquency,
+				x: Math.random() * width,
+				y: Math.random() * height
+			}
+		));
+
+		// nodes may have to be sorted
+		return myNodes
+	}
+
+	// higher order function that actually creates the chart/simulation
+	// this is what will be returned by the parent function
+	var chart = function (selector, data) {
+		nodes = createNodes(data);
+
+		svg = d3.select(selector)
+			.append('svg')
+			.attr('width', width)
+			.attr('height', height);
+
+		bubbles = svg.selectAll('.bubble')
+			.data(nodes);
+
+		var newBubbles = bubbles
+			.enter()
+			.append('circle')
+			.attr('r', 0)
+			.attr('fill', '#FF9900');
+
+		newBubbles.append("text")
+			.attr("dy", ".2em")
+			.style("text-anchor", "middle")
+			.text(d => d.word);
+		
+		bubbles = bubbles.merge(newBubbles);
+
+		bubbles.transition()
+			.duration(2000)
+			.attr('r', d => (d.radius));
+
+		simulation.nodes(nodes);
+		simulation.alpha(1).restart()
+	};
+
+	function ticked() {
+		bubbles
+		.attr('cx', d => (d.x))
+		.attr('cy', d => (d.y));
+	}
+
+	return chart
+
+}
+
+var myBubbleChart = createChart();
+
+
+
+
+function main(error, data) {
+	if (error) {
+		console.log(erorr);
+	}
+
+	myBubbleChart('#vis', data);
+}
+
+d3.json("../data/word_frequency.json", main)
